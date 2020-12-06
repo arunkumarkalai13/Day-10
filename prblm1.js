91946751ff4bf17890b4754da94a6c8be4038dfd
arunkumarkalai13 <75129159+arunkumarkalai13@users.noreply.github.com>
2020-12-06 21:49:22 +0530

    Update prblm1.js

--- prblm1.js
+++ prblm1.js
@@ -5,9 +5,9 @@ let current = date.getDate();
 let d =date.getDate()-date1.getDate()-1;
 while(d>0){
   date.setDate(current-d);
-  console.log(date.getDate(),date.getMonth()+1,date.getFullYear());
+  console.log(`${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`);
   d--;
 }
 date.setDate(current-1);
 }
-begetdatebetween('11/01/2020','11/10/2020');
+begetdatebetween('11/01/2020','11/03/2020');
